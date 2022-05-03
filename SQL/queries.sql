3ec8a822930c535c851a5d83a33e6a2fd79e68ca
heather <queenheather32@gmail.com>
2022-05-02 21:10:50 -0400

    Added Query for Surfers with location names

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -10,7 +10,7 @@ select	continent.continent,
         region.region,
         city.city
 from wsl.city city
-join wsl.region region on city.region_id = region.region_id
+join wsl.region region on break.region_id = region.region_id
 join wsl.country country on region.country_id = country.country_id
 join wsl.continent continent on continent.continent_id = country.continent_id
 
@@ -24,7 +24,32 @@ join wsl.region region on city.region_id = region.region_id
 join wsl.country country on region.country_id = country.country_id
 join wsl.continent continent on continent.continent_id = country.continent_id
 
+
 select * from wsl.surfers;
+
+select 	gender,
+		first_name,
+        last_name,
+        stance,
+        rep_country.country as rep_country,
+        current_date() - birthday as age,
+        height,
+        weight,
+        concat(first_season, ' ', first_tour) as rookie_year,
+        home_country.country as home_country,
+        home_region.region as home_region,
+        home_city.city as home_city
+from wsl.surfers surfers
+join wsl.country as rep_country
+		on rep_country.country_id = surfers.rep_country_id
+join wsl.city as home_city
+		on home_city.city_id = surfers.home_city_id
+join wsl.region as home_region
+		on home_region.region_id = home_city.region_id
+join wsl.country as home_country
+		on home_country.country_id = home_region.country_id
+;
+
 
 select * from wsl.tour;
 select * from wsl.event;
