f1649026b96a37d23d2c4adfe586181680046652
heather <queenheather32@gmail.com>
2022-05-20 16:41:24 -0400

    queries update

---
+++ src/vizzies/heat_summary.py
@@ -0,0 +1,17 @@
+# Data Processing for heat summaries
+# Visualizations for heat summaries
+#
+#######################################################################################################################
+# Imports
+from src.models import Tour, Event, Round, HeatDetails, HeatResults
+
+########################################################################################################################
+# Pull a Heat to display
+
+# Pull A Specific Heat
+entered_tour_name = '2022 Mens Championship Tour'
+entered_event_name = 'Billabong Pro Pipeline'
+entered_round_name = 'Opening Round'
+entered_heat_nbr = 1
+
+
